ce75211046ebad3e9bc09eb71e704c75d76cf393
Han <124041261+Gomyeunghan@users.noreply.github.com>
2026-07-14 14:35:54 +0900

    test : test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <div className="h-screen w-20 flex-col items-center justify-center flex">
-        <h1 className="text-4xl font-bold">Hello World</h1>
+      <div className="flex h-screen w-20 flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Hello World!</h1>
       </div>
     </>
   );
