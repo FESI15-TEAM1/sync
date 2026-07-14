e00e91b3cc13f293aa32725dcc26b0b545e70347
Han <124041261+Gomyeunghan@users.noreply.github.com>
2026-07-14 14:20:29 +0900

    chore : 프리티어 자동정렬 패키지 적용

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,13 +1,15 @@
 module.exports = {
-  arrowParens: "always",
-  htmlWhitespaceSensitivity: "css",
+  
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
   bracketSameLine: false,
   bracketSpacing: true,
-  proseWrap: "preserve",
-  quoteProps: "as-needed",
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
   semi: true,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma: "es5",
+  trailingComma: 'es5',
+  plugins: ["prettier-plugin-tailwindcss"],
   useTabs: false,
 };
