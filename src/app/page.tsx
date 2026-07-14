39dfdbda13e8dfea50fb9188f4dd8e28560166d0
Han <124041261+Gomyeunghan@users.noreply.github.com>
2026-07-14 14:19:37 +0900

    chore : esLint Import자동 정렬 패키지 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <div className="h-screen w-20 flex-col items-center justify-center flex">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </div>
+    </>
+  );
 }
