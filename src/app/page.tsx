db975d72a8bdef03197b82beadddff2c586c0c26
박민규 <aldrb0306@gmail.com>
2026-07-20 17:52:27 +0900

    chore: svgr 라이브러리 설치
    husky 비활성화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
-import { HomePage } from '@/components/pages/home-page';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Image from 'next/image';
+
+import Bell from '@/assets/icon/bell.svg'; // SVGR library
+import { HomePage } from '@/components/pages/home-page';
 
 export default function Home() {
   return (
     <>
-      <div className="flex h-screen w-20 flex-col items-center justify-center">
+      <div className="flex h-screen w-full flex-col items-center justify-center">
         <h1 className="text-4xl font-bold">Hello World!!</h1>
       </div>
 
       <HomePage />
+
+      <Bell width={20} height={20} />
+
+      <Image src="/bell.svg" alt="종 모양 아이콘" width={20} height={20} />
     </>
   );
 }
