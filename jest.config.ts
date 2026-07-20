8e2a28d164cc5dfafca331ae53cde11558c80586
Han <rhaudgks04@gmail.com>
2026-07-20 20:13:39 +0900

    test: svgr라이브러리 사용 후 jest테스트

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
   testEnvironment: 'jsdom',
   // 각 테스트가 실행되기 전에 추가 설정 옵션을 추가합니다.
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    // next/jest가 내부적으로 등록한 '^.+\\.(svg)$' 규칙과 동일한 키를 써야
+    // 새로 추가하는 게 아니라 덮어써서 svgr 목(mock)이 우선 적용됩니다.
+    '^.+\\.(svg)$': '<rootDir>/__mocks__/svg.tsx',
+  },
 };
 
 // next/jest가 Next.js 구성을 로드할 수 있도록 createJestConfig가 이 방식으로 내보내집니다.
