3f219d728d3c6247abb766802984cf07919ebeab
박민규 <aldrb0306@gmail.com>
2026-07-22 17:22:54 +0900

    chore: test page 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-'use client';
-
-import { useState } from 'react';
-
-import { HomePage } from '@/components/pages/home-page';
+import TestPage from './testPage';
 
 export default function Home() {
   return (
     <>
-      <div className="flex h-screen w-full flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold">Hello World!!</h1>
-        <h1 className="text-4xl font-bold">세상아 안녕??</h1>
-      </div>
-      
-      <HomePage />
+      <TestPage />
     </>
   );
 }
