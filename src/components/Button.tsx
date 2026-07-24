d604c4a1169b6afd8828186d5070deac516332dc
Han <rhaudgks04@gmail.com>
2026-07-24 10:57:38 +0900

    refactor: onClick함수 타입 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import clsx from 'clsx';
+import { type MouseEvent } from 'react';
 import { type ComponentPropsWithoutRef } from 'react';
 import { type ReactNode } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export default function Button({
   children,
-  onClick = () => console.log('click'),
+  onClick,
   size = 'md',
   isDisabled,
   variant = 'primary',
@@ -16,7 +17,7 @@ export default function Button({
 }: ComponentPropsWithoutRef<'button'> & {
   // ComponentPropsWithoutRef == 이컴포넌트 태그가 받을 수 있는 모든 속성 중 ref만 빼고 button테그의 속성을 사용한다.
   children: ReactNode;
-  onClick: () => void;
+  onClick: (e?: MouseEvent<HTMLButtonElement>) => void;
   size?: 'sm' | 'md' | 'lg';
   isDisabled: boolean;
   variant?: 'primary' | 'secondary' | 'outline';
