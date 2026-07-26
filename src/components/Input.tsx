476b02303439e06daea0796e162963a378d772cd
kimhyunjoo <khjoo1203@gmail.com>
2026-07-26 10:35:46 +0900

    feat: input 컴포넌트 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,42 +1,29 @@
 'use client';
 
-import { clsx } from 'clsx';
-import { type ChangeEvent, useState } from 'react';
-import { twMerge } from 'tailwind-merge';
+import { type InputHTMLAttributes, useState } from 'react';
 
 import EyeIcon from '@/assets/icons/eye.svg';
 import EyeOffIcon from '@/assets/icons/eye-off.svg';
 
-type InputProps = {
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
-  placeholder?: string;
-  value: string;
   width?: number | string;
   height?: number | string;
-  type?: 'text' | 'password' | 'email';
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   errorMessage?: string;
 };
 
-// 인풋 스타일 공유 사용!
-export const fieldStyle = twMerge(
-  clsx(
-    'border-border bg-bg-card placeholder:text-text-secondary w-full rounded-md border px-3 py-2 text-base text-white focus:outline-none',
-  ),
-);
-
 export default function Input({
   label,
-  placeholder,
-  value,
-  onChange,
-  width = 255,
-  height = 44,
+  width,
+  height,
   type = 'text',
   errorMessage,
+  ...props
 }: InputProps) {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
   const isPassword = type === 'password';
+
   const inputType = isPassword
     ? isPasswordVisible
       ? 'text'
@@ -45,18 +32,26 @@ export default function Input({
 
   return (
     <div className="flex w-full flex-col gap-1">
-      {label ? (
+      {label && (
         <label className="ml-2 text-base font-bold text-white">{label}</label>
-      ) : null}
-      <div className="relative" style={{ width, height }}>
+      )}
+
+      <div
+        className="relative"
+        style={{
+          width,
+          height,
+        }}
+      >
         <input
+          {...props}
           type={inputType}
-          className={`${fieldStyle} h-full ${isPassword ? 'pr-11' : ''}`}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
+          className={`border-border bg-bg-card placeholder:text-text-secondary h-full w-full rounded-md border px-3 py-2 text-base text-white focus:outline-none ${
+            isPassword ? 'pr-11' : ''
+          }`}
         />
-        {isPassword ? (
+
+        {isPassword && (
           <button
             type="button"
             aria-label={isPasswordVisible ? '비밀번호 숨기기' : '비밀번호 보기'}
@@ -69,11 +64,10 @@ export default function Input({
               <EyeIcon width={20} height={20} aria-hidden />
             )}
           </button>
-        ) : null}
+        )}
       </div>
-      {errorMessage ? (
-        <p className="text-sm text-red-500">{errorMessage}</p>
-      ) : null}
+
+      {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
     </div>
   );
 }
