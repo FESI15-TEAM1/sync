a7e8b5ab59ed175be17c0c666431ead031d0317a
kimhyunjoo <khjoo1203@gmail.com>
2026-07-27 18:46:25 +0900

    feat: 그룹 상세페이지에서 참여자/그룹장 로 나뉨

--- src/app/group/[id]/page.tsx
+++ src/app/group/[id]/page.tsx
@@ -10,6 +10,6 @@ export default async function GroupDetailPage({
   const { id } = await params;
   const { role } = await searchParams;
 
-  // TODO: 임시 — 리더 UI 확인용. 끝나면 role === 'leader' 로 되돌리기
-  return <GroupDetail groupId={id} isJoined />;
+  // TODO: 임시 — 리더 UI 확인용. 끝나면 role === 'leader' 로 되돌리기  isJoined도 추가함
+  return <GroupDetail groupId={id} isLeader />;
 }
