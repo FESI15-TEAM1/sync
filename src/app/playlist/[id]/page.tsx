92f7d356a69d48570b8e2b963eb658b49d19c5de
Han <rhaudgks04@gmail.com>
2026-08-05 15:25:49 +0900

    feat: playlist{id}페이지 api연결

--- src/app/playlist/[id]/page.tsx
+++ src/app/playlist/[id]/page.tsx
@@ -1,109 +1,43 @@
+import { serverFetch } from '@/lib/http/server-fetch';
+import type {
+  LikePlaylistResponse,
+  MyplaylistResponse,
+} from '@/services/playlist/playlistCard.type';
+
 import PlaylistView from './_components/PlaylistView';
 
-export const data = {
-  items: [
+export default async function Playlist({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const initalData = await serverFetch<MyplaylistResponse>(
+    `/users/${(await params).id}/playlists`,
     {
-      id: 1,
-      title: '비오는날 감성',
-      description: '빗소리와 잘 어울리는 잔잔한 플레이리스트',
-      image: 'https://picsum.photos/seed/rain1/400/400',
-      isPublic: true,
-      trackCount: 18,
-      createdAt: '2026-07-20T09:00:00Z',
+      method: 'GET',
     },
-    {
-      id: 2,
-      title: '출근길 텐션업',
-      description: '아침을 깨워주는 신나는 곡 모음',
-      image: 'https://picsum.photos/seed/morning2/400/400',
-      isPublic: true,
-      trackCount: 24,
-      createdAt: '2026-07-18T07:30:00Z',
-    },
-    {
-      id: 3,
-      title: '새벽 감성 R&B',
-      description: '잠 안 올 때 듣는 곡들',
-      image: 'https://picsum.photos/seed/night3/400/400',
-      isPublic: false,
-      trackCount: 12,
-      createdAt: '2026-07-15T23:15:00Z',
-    },
-    {
-      id: 4,
-      title: '카페에서 듣기 좋은 노래',
-      description: '잔잔하고 편안한 어쿠스틱 위주',
-      image: 'https://picsum.photos/seed/cafe4/400/400',
-      isPublic: true,
-      trackCount: 30,
-      createdAt: '2026-07-10T14:20:00Z',
-    },
-    {
-      id: 5,
-      title: '운동할 때 필수',
-      description: '고강도 운동용 EDM/힙합 믹스',
-      image: 'https://picsum.photos/seed/workout5/400/400',
-      isPublic: true,
-      trackCount: 20,
-      createdAt: '2026-07-05T18:00:00Z',
-    },
-  ],
-  nextCursor: null,
-};
+  );
 
-export const likedata = {
-  items: [
+  const initalLikeData = await serverFetch<LikePlaylistResponse>(
+    `/users/me/liked-playlists`,
     {
-      id: 1,
-      title: '비오는날 감성',
-      image: 'https://picsum.photos/seed/rain1/400/400',
-      isPublic: true,
-      trackCount: 18,
-      likeCount: 10,
-      owner: {
-        userId: 1,
-        nickname: 'User1',
-        image: 'https://picsum.photos/seed/user1/100/100',
-      },
-      createdAt: '2026-07-20T09:00:00Z',
+      method: 'GET',
     },
+  );
+  const userNickname = await serverFetch<{ id: number; nickname: string }>(
+    `/users/${(await params).id}`,
     {
-      id: 2,
-      title: '해질녘 감성',
-      image: 'https://picsum.photos/seed/rain1/400/400',
-      isPublic: true,
-      trackCount: 18,
-      likeCount: 10,
-      owner: {
-        userId: 2,
-        nickname: 'User1',
-        image: 'https://picsum.photos/seed/user1/100/100',
-      },
-      createdAt: '2026-07-20T09:00:00Z',
+      method: 'GET',
     },
-    {
-      id: 3,
-      title: '새벽 감성 R&B',
-      description: '잠 안 올 때 듣는 곡들',
-      image: 'https://picsum.photos/seed/night3/400/400',
-      isPublic: false,
-      trackCount: 12,
-      likeCount: 5,
-      owner: {
-        userId: 3,
-        nickname: 'User2',
-        image: 'https://picsum.photos/seed/user2/100/100',
-      },
-      createdAt: '2026-07-15T23:15:00Z',
-    },
-  ],
-  nextCursor: null,
-};
+  );
 
-export default function Playlist() {
   return (
     <div>
-      <PlaylistView myData={data.items} likedData={likedata.items} />
+      <PlaylistView
+        myData={initalData.items}
+        likedData={initalLikeData.items}
+        userNickname={userNickname.nickname}
+      />
     </div>
   );
 }
