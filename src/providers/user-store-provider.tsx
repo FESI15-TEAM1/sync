dd5b0b2cd7ecc47e99ea4b8fa3410b2703b606ee
kimhyunjoo <khjoo1203@gmail.com>
2026-08-05 11:03:11 +0900

    feat: getMe 삭제

--- src/providers/user-store-provider.tsx
+++ src/providers/user-store-provider.tsx
@@ -25,29 +25,6 @@ export interface UserStoreProviderProps {
 export const UserStoreProvider = ({ children }: UserStoreProviderProps) => {
   const [store] = useState(() => createUserStore());
 
-  // useEffect(() => {
-  //   getMe()
-  //     .then((me) =>
-  //       store.setState({
-  //         user: {
-  //           id: me.id,
-  //           nickname: me.nickname,
-  //           image: me.image,
-  //           email: me.email,
-  //         },
-  //         isLoading: false,
-  //       }),
-  //     )
-  //     .catch(() => {
-  //       // 로그인이 먼저 끝나 유저가 채워졌다면 이 조회의 실패로 덮어쓰지 않는다.
-  //       if (store.getState().user !== null) {
-  //         store.setState({ isLoading: false });
-  //         return;
-  //       }
-  //       store.setState({ user: null, isLoading: false });
-  //     });
-  // }, [store]);
-
   return (
     <UserStoreContext.Provider value={store}>
       {children}
