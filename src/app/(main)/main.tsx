d704385a44b6ec593cbf787bdb33f412cfa0a78a
박민규 <aldrb0306@gmail.com>
2026-08-06 13:43:14 +0900

    chore: playroom 리스트 생성 컴포넌트 분리로 인한 더미데이터 노출 수정

--- src/app/(main)/main.tsx
+++ src/app/(main)/main.tsx
@@ -1,10 +1,10 @@
 import Section from '@/app/(main)/_components/Section';
 import PlaylistCardList from '@/components/domain/playlists/PlaylistCardList';
-
-import PlayroomList from '../stage/_components/PlayroomList';
+import PlayroomList from '@/components/domain/playroom/PlayroomList';
+import { type PlayroomCardData } from '@/services/playroom/playroom.types';
 
 export default function Main() {
-  const playroomDummy = [
+  const playroomDummy: PlayroomCardData[] = [
     {
       id: 3,
       title: '새벽 감성방',
@@ -12,7 +12,7 @@ export default function Main() {
         '새벽 감성방에 오신 것을 환영합니다. 이 방은 새벽 감성을 공유하는 공간입니다.',
       hashtags: ['Jazz', 'Blues', 'Indie', 'Ballad', 'R&B'],
       listenerCount: 1,
-      host: '김디제이',
+      host: { userId: 1, nickname: '김디제이', image: null },
     },
     {
       id: 7,
@@ -21,7 +21,7 @@ export default function Main() {
         '창문 내리고 달릴 때 듣기 좋은 곡만 모았습니다. 신나는 노래 환영해요.',
       hashtags: ['Pop', 'Rock', 'Dance', 'City Pop'],
       listenerCount: 3,
-      host: '창문내린김대리',
+      host: { userId: 2, nickname: '창문내린김대리', image: null },
     },
     {
       id: 54,
@@ -30,7 +30,7 @@ export default function Main() {
         '가사 없는 음악만 틀어주세요. 조용히 각자 할 일 하는 작업 공간입니다.',
       hashtags: ['Lo-fi', 'Ambient', 'Classical', 'Piano'],
       listenerCount: 15,
-      host: '이어폰붙박이',
+      host: { userId: 3, nickname: '이어폰붙박이', image: null },
     },
   ];
 
@@ -90,10 +90,6 @@ export default function Main() {
       <Section
         headline="새로 생긴 플레이리스트!"
         list={<PlaylistCardList data={playlistDummy} />}
-      />
-      <Section
-        headline="최근 그룹🌻"
-        list={<PlayroomList data={playroomDummy} />}
       />
     </div>
   );
