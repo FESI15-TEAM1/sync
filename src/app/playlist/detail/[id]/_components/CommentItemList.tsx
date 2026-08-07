733095f8c1a99b14c1545d3acb54a57638a6ceef
Han <rhaudgks04@gmail.com>
2026-08-07 16:51:42 +0900

    feat: commentitem 에 사용될 props 추가

--- src/app/playlist/detail/[id]/_components/CommentItemList.tsx
+++ src/app/playlist/detail/[id]/_components/CommentItemList.tsx
@@ -19,16 +19,28 @@ export interface CommentItemType {
 export default function CommentItemList({
   comments,
   userid,
+  onEditSave,
+  isSaving,
 }: {
   comments: CommentItemsType;
   userid: string | number | null;
+  onEditSave: (commentId: number, content: string) => Promise<unknown>;
+  isSaving: boolean;
 }) {
   //author의 유저id와 현재 유저의 아이디가 동일하면 commentitem에 owner인지 아닌지 넘겨주기
   //
   return (
     <div className="flex flex-col gap-3">
       {[...comments.items].reverse().map((item) => {
-        return <CommentItem key={item.id} comment={item} userid={userid} />;
+        return (
+          <CommentItem
+            key={item.id}
+            comment={item}
+            userid={userid}
+            onEditSave={onEditSave}
+            isSaving={isSaving}
+          />
+        );
       })}
     </div>
   );
