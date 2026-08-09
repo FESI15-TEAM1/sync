44e72ffcdf0f6245450359aac20f453f24752022
Han <rhaudgks04@gmail.com>
2026-08-09 15:44:39 +0900

    fix: 비회원 사용자도 접근 가능하도록 변경

--- src/app/playlist/detail/[id]/page.tsx
+++ src/app/playlist/detail/[id]/page.tsx
@@ -1,15 +1,9 @@
-import { redirect } from 'next/navigation';
-
 import PlaylistDetailView from '@/app/playlist/detail/[id]/_components/PlaylistDetailView';
 import { APIError } from '@/lib/http/error';
 import { serverFetch } from '@/lib/http/server-fetch';
 
 export default async function PlaylistDeatilPage() {
   const userId = await getCurrentUserId();
-
-  if (userId === null) {
-    redirect('/login');
-  }
 
   return (
     <div className="flex justify-center p-6">
