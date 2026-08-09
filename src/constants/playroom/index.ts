a486e7f18cb1120987feae8da0d44286385c3b42
박민규 <aldrb0306@gmail.com>
2026-08-09 22:44:49 +0900

    chore: playroom 관련 상수 분리

---
+++ src/constants/playroom/index.ts
@@ -0,0 +1,7 @@
+/**
+ * 방 제목·설명 길이 제한. 백엔드 스펙(PlayroomUpdateRequest)과 동일한 값입니다.
+ *
+ * 사용처: src/app/playroom/[playroomId]/_components/PlayroomEditModal.tsx
+ */
+export const PLAYROOM_TITLE_MAX_LENGTH = 100;
+export const PLAYROOM_DESCRIPTION_MAX_LENGTH = 500;
