84854ce5fc373e0265f1d98a650092bca2c84a8c
Han <rhaudgks04@gmail.com>
2026-08-10 14:32:35 +0900

    feat: 좋아요 PUT,DELETE 라우터 헨들러 구현

---
+++ src/app/api/playlists/[id]/like/route.ts
@@ -0,0 +1,71 @@
+import { NextResponse } from 'next/server';
+
+import { APIError } from '@/lib/http/error';
+import { serverFetch } from '@/lib/http/server-fetch';
+
+export async function PUT(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    await serverFetch(`/playlists/${(await params).id}/like`, {
+      method: 'PUT',
+    });
+
+    return NextResponse.json({ status: 204 });
+  } catch (error) {
+    if (error instanceof APIError) {
+      return NextResponse.json(
+        {
+          error: {
+            code: error.code,
+            message: error.message,
+          },
+        },
+        { status: error.status },
+      );
+    }
+    return NextResponse.json(
+      {
+        error: {
+          code: 'INTERNAL_SERVER_ERROR',
+          message: '서버 오류가 발생하였습니다.',
+        },
+      },
+      { status: 500 },
+    );
+  }
+}
+export async function DELETE(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    await serverFetch(`/playlists/${(await params).id}/like`, {
+      method: 'DELETE',
+    });
+
+    return NextResponse.json({ status: 204 });
+  } catch (error) {
+    if (error instanceof APIError) {
+      return NextResponse.json(
+        {
+          error: {
+            code: error.code,
+            message: error.message,
+          },
+        },
+        { status: error.status },
+      );
+    }
+    return NextResponse.json(
+      {
+        error: {
+          code: 'INTERNAL_SERVER_ERROR',
+          message: '서버 오류가 발생하였습니다.',
+        },
+      },
+      { status: 500 },
+    );
+  }
+}
