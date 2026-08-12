1cf756224cd96d29b01fbf9e3324208831a4383b
Han <rhaudgks04@gmail.com>
2026-08-12 18:45:42 +0900

    fix: 빌드에러 useSearchParms사용으로 서버컴포넌트인 search를 Suspense로 감쌈

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,9 +1,19 @@
+import { Suspense } from 'react';
+
 import SearchForm from '@/app/search/_components/SearchForm';
 
 export default function Search() {
   return (
     <div className="w-full overflow-x-hidden">
-      <SearchForm />
+      <Suspense
+        fallback={
+          <div className="text-text-primary flex min-h-[60vh] w-full items-center justify-center font-bold">
+            로딩중..
+          </div>
+        }
+      >
+        <SearchForm />
+      </Suspense>
     </div>
   );
 }
