758f2f78e1706155e51415ad5cf0ded553915da6
kimhyunjoo <khjoo1203@gmail.com>
2026-08-18 18:11:42 +0900

    style: w값 제거

--- src/app/profile/[userId]/_components/Profile.tsx
+++ src/app/profile/[userId]/_components/Profile.tsx
@@ -132,7 +132,7 @@ export default function Profile(props: ProfileProps) {
 
   return (
     <>
-      <div className="mx-auto flex w-full max-w-md flex-1 flex-col gap-6 px-5 py-6">
+      <div className="mx-auto flex flex-1 flex-col gap-6 px-5 py-6">
         <div className="flex flex-col gap-5">
           {isOwn ? (
             <div className="flex flex-col items-end gap-1">
