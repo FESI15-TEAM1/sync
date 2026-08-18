ee5956edc0ccac7841109a292b4e78822cf552ac
kimhyunjoo <khjoo1203@gmail.com>
2026-08-18 18:07:07 +0900

    feat: count가 아니라 글씨에 hover 시 밑줄

--- src/app/profile/[userId]/_components/Profile.tsx
+++ src/app/profile/[userId]/_components/Profile.tsx
@@ -246,35 +246,41 @@ export default function Profile(props: ProfileProps) {
           <div className="flex justify-between">
             {isOwn ? (
               <Link href="/group" className="text-center">
-                <p className="text-text-primary font-bold hover:underline">
+                <p className="text-text-primary font-bold">
                   {props.profile.groupCount}
                 </p>
-                <p className="text-text-secondary mt-1 text-xs">내 그룹</p>
+                <p className="text-text-secondary mt-1 text-xs hover:underline">
+                  내 그룹
+                </p>
               </Link>
             ) : null}
             <Link href={`/playlist/${profile.id}`} className="text-center">
-              <p className="text-text-primary font-bold hover:underline">
+              <p className="text-text-primary font-bold">
                 {profile.playlistCount}
               </p>
-              <p className="text-text-secondary mt-1 text-xs">플레이리스트</p>
+              <p className="text-text-secondary mt-1 text-xs hover:underline">
+                플레이리스트
+              </p>
             </Link>
             <Link
               href={`/profile/${profile.id}/follow`}
               className="text-center"
             >
-              <p className="text-text-primary font-bold hover:underline">
-                {followerCount}
+              <p className="text-text-primary font-bold">{followerCount}</p>
+              <p className="text-text-secondary mt-1 text-xs hover:underline">
+                팔로우
               </p>
-              <p className="text-text-secondary mt-1 text-xs">팔로우</p>
             </Link>
             <Link
               href={`/profile/${profile.id}/follow?tab=following`}
               className="text-center"
             >
-              <p className="text-text-primary font-bold hover:underline">
+              <p className="text-text-primary font-bold">
                 {profile.followingCount}
               </p>
-              <p className="text-text-secondary mt-1 text-xs">팔로잉</p>
+              <p className="text-text-secondary mt-1 text-xs hover:underline">
+                팔로잉
+              </p>
             </Link>
           </div>
 
