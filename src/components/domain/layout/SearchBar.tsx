933b74084c1ea34d580f09e4bdbfd4b46079c15b
박민규 <aldrb0306@gmail.com>
2026-08-19 13:09:33 +0900

    fix: 검색바 동작 수정
    input 자체에서 keydown 이벤트 대신 form 으로 감싸서 submit 처리로 수정하였습니다

--- src/components/domain/layout/SearchBar.tsx
+++ src/components/domain/layout/SearchBar.tsx
@@ -3,6 +3,7 @@
 import { cva } from 'class-variance-authority';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { type SubmitEvent } from 'react';
 
 import Serach from '@/assets/icons/Search.svg';
 
@@ -23,26 +24,29 @@ export default function SearchBar() {
     },
   );
 
-  const handleSearch = () => {
+  const handleSearch = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
     if (!query.trim()) return;
     router.push(`/search?q=${encodeURIComponent(query)}`);
   };
 
   return (
-    <div className={inputStyle({ isFocused })}>
+    <form className={inputStyle({ isFocused })} onSubmit={handleSearch}>
       <input
         className="w-full bg-transparent text-sm outline-none"
         placeholder="플레이리스트, 그룹 검색"
         onChange={(e) => setQuery(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter' && !e.nativeEvent.isComposing) handleSearch();
-        }}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
       ></input>
-      <button className="shrink-0 cursor-pointer" onClick={handleSearch}>
+      <button
+        className="shrink-0 cursor-pointer"
+        type="submit"
+        aria-label="검색"
+      >
         <Serach />
       </button>
-    </div>
+    </form>
   );
 }
