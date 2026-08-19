fc33995ff7195cab7c0973581dbdfa34fbb51d65
Han <rhaudgks04@gmail.com>
2026-08-19 15:03:06 +0900

    style: 내 그룹 스타일 수정

--- src/app/group/[id]/_components/GroupJoinBanner.tsx
+++ src/app/group/[id]/_components/GroupJoinBanner.tsx
@@ -22,7 +22,7 @@ export default function GroupJoinBanner({
   if (!group.isPublic || isLeader || group.isMember) return null;
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="mx-auto flex w-1/2 flex-col gap-2">
       <Button
         type="button"
         variant="primary"
