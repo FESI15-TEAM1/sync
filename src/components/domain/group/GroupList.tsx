faa6592eff5dca397b9fb711d493c07d3f3acf57
kimhyunjoo <khjoo1203@gmail.com>
2026-08-19 10:34:53 +0900

    feat: 메인페이지 그룹 hover수정

--- src/components/domain/group/GroupList.tsx
+++ src/components/domain/group/GroupList.tsx
@@ -8,7 +8,10 @@ export default function GroupList({ data }: { data: GroupSummary[] }) {
     <ul className="flex flex-col gap-3">
       {data.map((group) => (
         <li key={group.id}>
-          <Link href={`/group/${group.id}`} className="block w-full">
+          <Link
+            href={`/group/${group.id}`}
+            className="hover:bg-input block w-full rounded-2xl transition-all hover:-translate-y-0.5 hover:shadow-xl"
+          >
             <GroupListItem {...group} />
           </Link>
         </li>
